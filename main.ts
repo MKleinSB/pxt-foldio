eba3c6c29f92cde6dd0425e5208787996c5433b4
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-02-16 19:42:50 +0100

    first release

--- main.ts
+++ main.ts
@@ -1,7 +1,63 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Custom blocks
+ */
+//% weight=120 color=#0fbc11 icon=""
+namespace Foldio {
+    /**
+ 
+     */
+    //% blockId=device_foldio_leftEar block="Wenn linkes Ohr berührt wird"
+    //%blockExternalInputs=false
+    export function lTouch(body: Action): void {
+        input.onPinPressed(TouchPin.P0, body)
+    }
+
+    /**
+  
+     */
+    //% blockId=device_foldio_rightEar block="Wenn rechtes Ohr berührt wird"
+    //%blockExternalInputs=false
+    export function rTouch(body: Action): void {
+        input.onPinPressed(23, body) //need to be changes to compile correct
+    }
+
+    /**
+     
+     */
+    //% blockId=device_foldio_blinken block="َAugen blinken lassen"
+    //%blockExternalInputs=false
+    export function Blink(): void {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+
+
+
+
+}
