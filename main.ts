0d805db5c1bfb812dc99a2c11e2816a00d945ef9
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-09-23 16:13:18 +0200

    Aktualisierungen.

--- main.ts
+++ main.ts
@@ -26,34 +26,50 @@ namespace Foldio {
      */
     //% blockId=device_foldio_blinken block="َAugen blinken lassen"
     //%blockExternalInputs=false
-    export function Blink(): void {
+    export function blinken() {
         basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        # # . # #
+        # # . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
         basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        # # . # #
+        # # . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     }
+    /**
+      * Ein kleines Geräusch machen
+      * @param n describe parameter here, eg: 5
+      * @param s describe parameter here, eg: "Hello"
+      * @param e describe parameter here
+      */
+    //% blockId=device_foldio_sprechen block="َSprechen"
+    //% weight=96 blockGap=8
+    //%blockExternalInputs=false
+    export function speak(): void {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(659, music.beat(BeatFraction.Whole))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+    }
+
 }
